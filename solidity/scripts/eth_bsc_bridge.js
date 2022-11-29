b15d917a467c45dd780dcfc9fb114b1b2a9c44fd
Min-Qi-Zhang <52721450+Min-Qi-Zhang@users.noreply.github.com>
2022-11-29 15:48:33 -0500

    Bidirectional bridge working

--- solidity/scripts/eth_bsc_bridge.js
+++ solidity/scripts/eth_bsc_bridge.js
@@ -9,38 +9,60 @@ const BridgeContract = require("../artifacts/contracts/Bridge.sol/Bridge.json");
 
 const ethProvider = new ethers.providers.WebSocketProvider(GOERLI_WSS);
 const bscProvider = new ethers.providers.JsonRpcProvider('https://data-seed-prebsc-1-s1.binance.org:8545');
-const wallet = new ethers.Wallet(PRIVATE_KEY, bscProvider);
+const ethWallet = new ethers.Wallet(PRIVATE_KEY, ethProvider);
+const bscWallet = new ethers.Wallet(PRIVATE_KEY, bscProvider);
 
 const originContract = new ethers.Contract(ORIGIN_CONTRACT_ADDRESS, BridgeContract.abi, ethProvider);
 const destContract = new ethers.Contract(DEST_CONTRACT_ADDRESS, BridgeContract.abi, bscProvider);
-const destContractWithSigner = destContract.connect(wallet);
+const origContractWithSigner = originContract.connect(ethWallet);
+const destContractWithSigner = destContract.connect(bscWallet);
 
-messageSent = []
-messageBatchSize = 4
+let messageSentByOrig = [];
+let messageSentByDest = [];
+let messageBatchSize = 4;
+
 /**
- * Record every message sent
+ * Record every message sent by origin
  */
-const recordMessageSent = async (messageId, sender, message, messageIndex, proof, rootHash, event) => {
+const recordMessageSentByOrig = async (messageId, sender, message, messageIndex, proof, rootHash, event) => {
     try {
         console.log("New message on source chain: ", JSON.stringify({ messageId, sender, message }));
-        messageSent.push({proof, rootHash, messageIndex, messageId, sender, message})
-        if (messageSent.length == messageBatchSize) {
-            for (let messageIndex = 0; messageIndex < messageSent.length; messageIndex++) {
-                await sendMessage(messageSent[messageIndex]);
+        messageSentByOrig.push({proof, rootHash, messageIndex, messageId, sender, message})
+        if (messageSentByOrig.length == messageBatchSize) {
+            for (let messageIndex = 0; messageIndex < messageSentByOrig.length; messageIndex++) {
+                await sendMessage(destContractWithSigner, messageSentByOrig[messageIndex]);
             }
-            messageSent = []
+            messageSentByOrig = [];
         }
     } catch (err) {
         console.log(err);
     }
 }
 
-const sendMessage = async ({proof, rootHash, messageIndex, messageId, sender, message}) => {
+/**
+ * Record every message sent by destination
+ */
+ const recordMessageSentByDest = async (messageId, sender, message, messageIndex, proof, rootHash, event) => {
+    try {
+        console.log("New message on dest chain: ", JSON.stringify({ messageId, sender, message }));
+        messageSentByDest.push({proof, rootHash, messageIndex, messageId, sender, message})
+        if (messageSentByDest.length == messageBatchSize) {
+            for (let messageIndex = 0; messageIndex < messageSentByDest.length; messageIndex++) {
+                await sendMessage(origContractWithSigner, messageSentByDest[messageIndex]);
+            }
+            messageSentByDest = [];
+        }
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+const sendMessage = async (contractWithSigner, {proof, rootHash, messageIndex, messageId, sender, message}) => {
     try {
         console.log("Sending new message to target chain: ", JSON.stringify({ proof, rootHash, messageIndex, messageId, sender, message}));
 
         console.log("Preparing for transaction...");
-        const tx = await destContractWithSigner.receiveMessage(proof, rootHash, messageIndex, messageId, sender, message);
+        const tx = await contractWithSigner.receiveMessage(proof, rootHash, messageIndex, messageId, sender, message);
         await tx.wait();
 
         console.log(tx);
@@ -51,4 +73,5 @@ const sendMessage = async ({proof, rootHash, messageIndex, messageId, sender, me
 }
 
 console.log("Listening to SendMessage event...");
-originContract.on("MessageSent", recordMessageSent);
+originContract.on("MessageSent", recordMessageSentByOrig);
+destContract.on("MessageSent", recordMessageSentByDest);
